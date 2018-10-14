a5351452f7511a01f289ad42a8de347636cfbd73
nealalan <neal@nealalan.com>
2018-10-14 15:41:33 -0600

    v2.1

--- quizv2.1.py
+++ quizv2.1.py
@@ -1,21 +1,15 @@
 ###########################################################################
-#  QUIZ SHUT OFF IN THIS VERSION TO DEBUG INPUT!!!!
-#
 #
 # python program to give a quiz based on two input files
 #
-# updated: 2018/10/11
+# updated: 2018/10/14
 # project: https://nealalan.github.io/quiz-giver-sec-plus
 #
-# bugs:
-#   - currently not handling answers with multiple letters correctly
-#     if the answer is AD the only answer that is accepted as correct
-#     is A (2018-10-10)
-#   - answer key is putting in crap because regex is pulling more than the first instance of the regex_pattern
 
 import re
 import random
 import string
+from sys import stdout
 
 ###########################################################################
 # INPUT FILES
@@ -30,10 +24,10 @@ QUESTION_NUMBER_PATTERN="^[0-9]{1,6}"
 QUESTION_NUMBER_PATTERN_NUMBER="\b\d+\b"
 QUESTION_NUMBER_ONLY="^[0-9]"
 # potential answers the user would enter
-ANSWER_LIST=['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f']
+ANSWER_LIST=['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'AB', 'ab', 'AC', 'ac', 'ACE', 'ace', 'AD', 'ad', 'ACD', 'acd', 'BC', 'bc', 'BD', 'bd', 'CE', 'ce', 'DE', 'de', 'ABE', 'abe', 'ABF', 'abf']
 # need logic to handle 1-3 possible answers at once
-SINGLE_LETTER_ANSWER_AFTER_NUMBER="[{A-F}]+[\\.]"
-SINGLE_LETTER_ANSWER_NUMBER_ONLY="[{A-F}]"
+LETTER_ANSWER_AFTER_NUMBER="[{A-F}]+[\\.]"
+LETTER_ANSWER_ONLY="[{A-F}]"
 YES=['Y', 'y']
 
 questions = []
@@ -112,6 +106,7 @@ def extract_questions(file_name):
         # start parsing the file
         # and writing it the the questions[] appropriately
         for line in f:
+            # FIND LINES OF TEXT THAT ARE A QUESTION
             if question_regex.match(line):
                 read_count += 1
                 # this is a silly way to do it - "find all numbers as strings in an array"
@@ -121,8 +116,9 @@ def extract_questions(file_name):
                 # Note: The line number is not a consecutive int in the quiz data!
                 line_number = int(filter(str.isdigit, line_number_string[0]))
                 questions.append(create_question(line_number,line))
-            # regex expression didn't match a number so hopefully, there is an answer!
+            # FINE LINES OF TEXT THAT ARE AN ANSWER CHOICE FOR THE QUESTION
             elif answer_regex.match(line):
+                # write to the array of questions in the next position
                 questions[len(questions) - 1]['choices'].append(create_answer(line))
     print('QUESTIONES ADDED: ' + str(read_count))
     return questions
@@ -138,7 +134,8 @@ def extract_key(file_name):
     with open(file_name) as f:
         # create the search parameters
         question_regex = re.compile(QUESTION_REGEX_PATTERN)
-        right_letter = re.compile(SINGLE_LETTER_ANSWER_AFTER_NUMBER)
+        correct_answer_letter_regex = re.compile(LETTER_ANSWER_AFTER_NUMBER)
+
         for line in f:
             # if the Q & A are out of order, print and check through
             #print questions[line_number]
@@ -146,14 +143,15 @@ def extract_key(file_name):
             if question_regex.match(line):
                 read_count += 1
                 # same dirty number extraction as above
-                line_number_string = re.findall(QUESTION_NUMBER_PATTERN,line)
+                line_number_string = question_regex.findall(line)
                 line_number = int(filter(str.isdigit, line_number_string[0]))
                 # THIS CODE NEEDS TO BE UPDATED TO HANDLE MULTPLE LETTER ANSWERS
-                correct_letter_a = right_letter.findall(line,0,15)
-                print(str(line_number) + ': ' + str(correct_letter_a))
-                correct_letter = re.findall(SINGLE_LETTER_ANSWER_NUMBER_ONLY,correct_letter_a[0])
+                correct_letter_a = correct_answer_letter_regex.findall(line,0,15)
+                #print(str(line_number) + ': ' + str(correct_letter_a))
+                correct_letter = re.findall(LETTER_ANSWER_ONLY,correct_letter_a[0])
+                #print(str(line_number) + ': ' + str(correct_letter))
                 answer_key.append(create_key(line_number,correct_letter,line))
-    print('ANSWERS ADDED: ' + str(line_number))
+    #print('ANSWERS ADDED: ' + str(line_number))
     return answer_key
 
 ###########################################################################
@@ -206,6 +204,7 @@ def quiz():
     wrong = 0
     right = 0
     continue_quiz = True
+    letter_regex = re.compile(LETTER_ANSWER_ONLY)
     while continue_quiz:
         number += 1
         infinity_loop_check = 0
@@ -223,22 +222,35 @@ def quiz():
             infinity_loop_check += 1
         if continue_quiz is False:
             break
+        # print the random question and associated choices to the output
         print("\n\033[1;33;39m" + current_question['question'] + " \033[0m ")
         for choice in current_question['choices']:
             print("\033[1;33;39m" + choice['choice'] + " \033[0m ")
+        # read the answer key
+        current_answer_key = check_answer_key(current_question_record)
+        print(len(current_answer_key['correct_answer']))
+        if len(current_answer_key['correct_answer']) > 1:
+            print('NOTE: THIS IS A MULTI ANSWER QUESTION!')
+        # ask the user for the answer
         get_answer = raw_input('ENTER YOUR ANSWER: ')
+        get_answer = string.upper(get_answer)
+        #
         while get_answer not in ANSWER_LIST:
             get_answer = raw_input('\033[5;35;38mENTER YOUR ANSWER:\033[0m ')
-        get_answer = string.upper(get_answer)
-        # read the answer key and print the answer
-        current_answer_key = check_answer_key(current_question_record)
-        if get_answer == current_answer_key['correct_answer'][0]:
+            get_answer = string.upper(get_answer)
+        # extract the answers in "AB" format to be in ['A', 'B'] format to compare
+        get_answer = letter_regex.findall(get_answer)
+        if get_answer == current_answer_key['correct_answer']:
             print("\n\033[5;31;38mCONGRATS, YOU'RE RIGHT! \033[0m ")
             right += 1
         else:
-            print('   CORRECT ANSWER: ' + current_answer_key['correct_answer'][0])
+            # write out in the format: CORRECT ANSWER: AB
+            stdout.write('   CORRECT ANSWER:' + ' ')
+            for letter in current_answer_key['correct_answer']:
+                stdout.write(letter)
             wrong += 1
-        print("\033[1;33;39m" + current_answer_key['explanation'] + " \033[0m ")
+
+        print("\n\033[1;33;39m" + current_answer_key['explanation'] + " \033[0m ")
         get_answer = raw_input("PRESS ENTER TO CONTINUE OR X TO EXIT")
         if get_answer in ['X','x','Q','q']:
             continue_quiz = False
@@ -253,5 +265,5 @@ if __name__ == '__main__':
     answer_key = extract_key(FILE_NAME2)
     question_range()
     #print_all_questions()
-    #quiz()
+    quiz()
     print("\nBYE")
